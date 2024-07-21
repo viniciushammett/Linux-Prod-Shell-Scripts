05fb30da6fe99a0526503eb8d4f5cc7f88cc7809
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2024-07-21 18:45:37 -0300

    Data Cleanup Script

---
+++ data-cleanup-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+directory="/path/to/cleanup"
+
+# Remove files older than 7 days in specified directory
+
+find "$directory" -type f -mtime +7 -exec rm {} \;
+echo "Old files removed."
