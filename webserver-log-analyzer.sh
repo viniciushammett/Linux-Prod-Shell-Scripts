960c6e92c7c2ad7a212ff660fa9aaa2a28ae9ca4
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2024-07-21 18:56:57 -0300

    Web Server Log Analyzer Script

---
+++ webserver-log-analyzer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+log_file="/var/log/apache2/access.log"
+
+# Analyze web server log to count unique IP addresses
+
+awk '{print $1}' "$log_file" | sort | uniq -c | sort -nr
+echo "Web server log analyzed."
